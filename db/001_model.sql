b88c10b3c2985d1c260e4fbd9113dae70a750c05
Scott Wales <scott.wales@unimelb.edu.au>
2017-05-08 17:32:07 +1000

    Updates to enable overriding the version number

--- db/001_model.sql
+++ db/001_model.sql
@@ -1,3 +1,4 @@
+/* Generic CF metadata */
 CREATE MATERIALIZED VIEW cf_attributes TABLESPACE ceph AS
     SELECT
         md_hash
@@ -8,9 +9,11 @@ CREATE MATERIALIZED VIEW cf_attributes TABLESPACE ceph AS
         metadata 
     WHERE
         md_json->'attributes'->>'Conventions' is not null;
-
 CREATE UNIQUE INDEX ON cf_attributes (md_hash);
 
+/* Metadata from the file specific to CMIP5 
+ * Gets the attributes out of JSON format into a more usable format
+ */
 CREATE MATERIALIZED VIEW cmip5_attributes TABLESPACE ceph AS
     SELECT
         md_hash
@@ -26,15 +29,11 @@ CREATE MATERIALIZED VIEW cmip5_attributes TABLESPACE ceph AS
       , md_json->'attributes'->>'realization'           as realization
       , md_json->'attributes'->>'initialization_method' as initialization_method
       , md_json->'attributes'->>'physics_version'       as physics_version
-      , 'r' || md_json->'attributes'->>'realization' ||
-        'i' || md_json->'attributes'->>'initialization_method' ||
-        'p' || md_json->'attributes'->>'physics_version' as ensemble_member
     FROM metadata 
     WHERE
         md_json->'attributes'->>'Conventions' is not null
       AND
         md_json->'attributes'->>'project_id' = 'CMIP5';
-
 CREATE UNIQUE INDEX ON cmip5_attributes (md_hash);
 CREATE INDEX ON cmip5_attributes (
     experiment_id
@@ -44,55 +43,89 @@ CREATE INDEX ON cmip5_attributes (
   , frequency
     );
 
-CREATE MATERIALIZED VIEW cmip5_dataset TABLESPACE ceph AS
+/* Derived CMIP5 attributes that are not specified in the file
+ * Includes links to normalised tables and 'rXiYpZ' ensemble member
+ */
+CREATE MATERIALIZED VIEW cmip5_attributes_derived TABLESPACE ceph AS
     SELECT
-        ROW_NUMBER() OVER() AS id
-      , s.experiment_id
-      , s.institute_id
-      , s.model_id
-      , s.modeling_realm
-      , s.frequency
-    FROM (SELECT DISTINCT
-        experiment_id
+        md_hash
+      , ensemble_member
+      , dataset_id
+      , md5 ( dataset_id || ':' || COALESCE(version_number,'-') ) as version_id
+    FROM (SELECT
+        md_hash
+      , version_number
+      , ('r' || realization ||
+        'i' || initialization_method ||
+        'p' || physics_version) as ensemble_member
+      , md5 ( experiment_id 
+        || ':' || institute_id 
+        || ':' || model_id 
+        || ':' || modeling_realm 
+        || ':' || frequency) as dataset_id
+    FROM cmip5_attributes) AS s;
+CREATE UNIQUE INDEX ON cmip5_attributes_derived (md_hash);
+CREATE INDEX ON cmip5_attributes_derived (dataset_id);
+CREATE INDEX ON cmip5_attributes_derived (version_id);
+
+/* A CMIP5 dataset
+ * Like what you find on ESGF, however version_number is broken out into its
+ * own table
+ */
+CREATE MATERIALIZED VIEW cmip5_dataset TABLESPACE ceph AS
+    SELECT DISTINCT
+        dataset_id
+      , experiment_id
       , institute_id
       , model_id
       , modeling_realm
       , frequency
-    FROM cmip5_attributes) as s;
-CREATE UNIQUE INDEX ON cmip5_dataset (id);
+    FROM cmip5_attributes NATURAL INNER JOIN cmip5_attributes_derived;
+CREATE UNIQUE INDEX ON cmip5_dataset (dataset_id);
 
-CREATE MATERIALIZED VIEW cmip5_version TABLESPACE ceph AS
-    SELECT
-        ROW_NUMBER() OVER() AS id
+/* The version number specified in the file
+ * This may not be accurate, so can be overridden by adding an entry to
+ * `cmip5_override_version`
+ * Normally the `cmip5_version` table should be used instead, as this contains
+ * fixed values
+ */ 
+CREATE MATERIALIZED VIEW cmip5_file_version TABLESPACE ceph AS
+    SELECT DISTINCT
+        md5 ( dataset_id || ':' || COALESCE(version_number,'-') ) as version_id
       , dataset_id
       , version_number
-    FROM cmip5_version_distinct;
+    FROM cmip5_attributes NATURAL INNER JOIN cmip5_attributes_derived;
+CREATE UNIQUE INDEX ON cmip5_file_version (version_id);
+CREATE INDEX ON cmip5_file_version (dataset_id);
 
-CREATE UNIQUE INDEX ON cmip5_version (id);
+/* Manually entered version information, for the case when the file is
+ * inaccurate or missing data
+ */
+CREATE TABLE cmip5_override_version(
+    version_id TEXT PRIMARY KEY,
+    version_number TEXT,
+    is_latest BOOLEAN,
+    to_update BOOLEAN) TABLESPACE ceph;
+CREATE UNIQUE INDEX ON cmip5_override_version (version_id);
 
-CREATE TABLE cmip5_version_extra TABLESPACE ceph (
-    id SERIAL PRIMARY KEY
-  , experiment_id TEXT
-  , institute_id TEXT
-  , model_id TEXT
-  , modeling_realm TEXT
-  , frequency TEXT
-  , version_number TEXT
-  , is_latest BOOLEAN
-    );
+/* View combining the file and override versions to provide a consistent view
+ */
+CREATE VIEW cmip5_version AS
+    SELECT
+        f.version_id as version_id
+      , f.dataset_id as dataset_id
+      , COALESCE(o.version_number, f.version_number) as version_number
+      , COALESCE(o.is_latest, FALSE) as is_latest
+    FROM cmip5_file_version AS f
+    LEFT JOIN cmip5_override_version AS o ON (f.version_id = o.version_id);
 
-CREATE UNIQUE INDEX ON cmip5_version_extra (
-    experiment_id
-  , institute_id
-  , model_id
-  , modeling_realm
-  , frequency
-  , version_number)
-
-
-CREATE TABLE warnings (
+/* Warnings associated with a dataset version
+ */
+CREATE TABLE cmip5_warnings (
     id SERIAL PRIMARY KEY,
+    version_id TEXT,
     warning TEXT,
     added_by TEXT,
     added_on DATE
-    );
+    ) TABLESPACE ceph;
+CREATE INDEX cmip5_warnings (version_id);
