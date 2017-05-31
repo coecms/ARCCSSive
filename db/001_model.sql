6b7c87a0593dbf29b10d2ffd7782bdf4d8cbda96
Scott Wales <scott.wales@unimelb.edu.au>
2017-05-31 12:57:52 +1000

    Split out cmip5 table into from-file and generated

--- db/001_model.sql
+++ db/001_model.sql
@@ -61,46 +61,43 @@ CREATE INDEX ON cf_variable_link(variable_id);
 /* Metadata from the file specific to CMIP5 
  * Gets the attributes out of JSON format into a more usable format
  */
-CREATE MATERIALIZED VIEW IF NOT EXISTS cmip5_attributes AS
-    WITH attrs AS (
-        SELECT
-            md_hash
-          , md_json->'attributes'->>'experiment_id'         as experiment_id
-          , md_json->'attributes'->>'frequency'             as frequency
-          , md_json->'attributes'->>'institute_id'          as institute_id
-          , md_json->'attributes'->>'model_id'              as model_id
-          , md_json->'attributes'->>'modeling_realm'        as modeling_realm
-          , md_json->'attributes'->>'product'               as product
-          , md_json->'attributes'->>'table_id'              as table_id
-          , md_json->'attributes'->>'tracking_id'           as tracking_id
-          , md_json->'attributes'->>'version_number'        as version_number
-          , md_json->'attributes'->>'realization'           as realization
-          , md_json->'attributes'->>'initialization_method' as initialization_method
-          , md_json->'attributes'->>'physics_version'       as physics_version
-        FROM metadata 
-        JOIN paths ON md_hash = pa_hash
-        WHERE
-            md_json->'attributes'->>'Conventions' IS NOT NULL
-          AND
-            md_json->'attributes'->>'project_id' = 'CMIP5'
-          AND (
-                pa_path LIKE '/g/data1/ua6/unofficial-ESG-replica/%'
-            OR
-                pa_path LIKE '/g/data1/ua6/authorative/%'
-            OR
-                pa_path LIKE '/g/data1/ua6/drstree/%'
-            OR
-                pa_path LIKE '/g/data1/ua6/DRSv2'
-          ) 
-    )
+CREATE MATERIALIZED VIEW IF NOT EXISTS cmip5_attributes_raw AS
+    SELECT
+        metadata.md_hash                                as md_hash
+      , md_json->'attributes'->>'experiment_id'         as experiment_id
+      , md_json->'attributes'->>'frequency'             as frequency
+      , md_json->'attributes'->>'institute_id'          as institute_id
+      , md_json->'attributes'->>'model_id'              as model_id
+      , md_json->'attributes'->>'modeling_realm'        as modeling_realm
+      , md_json->'attributes'->>'product'               as product
+      , md_json->'attributes'->>'table_id'              as table_id
+      , md_json->'attributes'->>'tracking_id'           as tracking_id
+      , md_json->'attributes'->>'version_number'        as version_number
+      , md_json->'attributes'->>'realization'           as realization
+      , md_json->'attributes'->>'initialization_method' as initialization_method
+      , md_json->'attributes'->>'physics_version'       as physics_version
+    FROM metadata 
+    JOIN paths ON metadata.md_hash = paths.pa_hash
+    WHERE
+        md_json->'attributes'->>'Conventions' IS NOT NULL
+      AND
+        md_json->'attributes'->>'project_id' = 'CMIP5'
+      AND pa_parents[4] IN (
+        md5('/g/data1/ua6/unofficial-ESG-replica')::uuid
+      , md5('/g/data1/ua6/authorative')::uuid
+      , md5('/g/data1/ua6/drstree')::uuid
+      , md5('/g/data1/ua6/DRSv2')::uuid
+      );
+CREATE UNIQUE INDEX ON cmip5_attributes_raw (md_hash);
+
+CREATE VIEW cmip5_attributes AS
     SELECT
         *
       , split_part(table_id, ' ', 2) as mip_table
       , ('r' || realization ||
          'i' || initialization_method ||
          'p' || physics_version) as ensemble_member
-    FROM attrs;
-CREATE UNIQUE INDEX ON cmip5_attributes (md_hash);
+    FROM cmip5_attributes_raw;
 
 /* Links to derived tables
  */
