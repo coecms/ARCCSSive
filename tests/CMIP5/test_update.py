b77d00ebaddaef7efe6636ff031ca279b0b58488
Scott Wales <scott.wales@unimelb.edu.au>
2017-06-06 16:04:55 +1000

    Fix update tests

--- tests/CMIP5/test_update.py
+++ tests/CMIP5/test_update.py
@@ -20,38 +20,49 @@ limitations under the License.
 from ARCCSSive.CMIP5.update_db_functions import *
 from ARCCSSive.CMIP5.Model import Instance, Version, VersionFile
 from .db_fixture import session
+import pytest
+
+test_search = {
+        'variable': 'thetao',
+        'experiment': 'rcp45',
+        'mip': 'Omon',
+        'model': 'GFDL-CM2p1',
+        'ensemble': 'r3i1p1',
+        'realm': 'ocean',
+        }
 
 def test_search_item(session):
     db=session.session 
-    kwargs={'variable': 'a', 'mip': '6hrLev', 'model': 'c',
-            'experiment': 'd', 'ensemble': 'e','realm':  'realm'}
-    item = search_item(db,Instance,**kwargs)
+    item = search_item(db,Instance,**test_search)
     assert item
 
-#def test_insert_unique(session):
-#    db=session.session
-#    kwargs={'variable': 'a2', 'mip': '6hrLev', 'model': 'c',
-#            'experiment': 'd', 'ensemble': 'e','realm':  'realm'}
-#    item, new= insert_unique(db,Instance,**kwargs)
-#    assert new is True
-#    assert type(item.id) is int 
-#
-# def test_update_item(session):
-#     db=session.session
-#     item_id=2
-#     item = search_item(db,Version,**{'id': item_id})
-#     assert item.is_latest is False
-#     newvalues={'is_latest': True}
-#     update_item(db, Version, item_id, newvalues)
-#     item = search_item(db,Version,**{'id': item_id})
-#     assert item.is_latest is True
-# 
-# def test_add_bulk_items(session):
-#     db=session.session
-#     vid=1
-#     num1=session.query(VersionFile).filter(VersionFile.version_id==vid).count() 
-#     data=[('f1.nc','s2it4'),('f2.nc','3grt4'),('f3.nc','3abt4'),('f4.nc','3gr32')]
-#     rows=[dict(filename=x[0], md5=x[1], version_id=vid) for x in data]
-#     add_bulk_items(db,VersionFile,rows)
-#     num2=session.query(VersionFile).filter(VersionFile.version_id==vid).count() 
-#     assert num2==num1+4
+@pytest.mark.skip(reason='Insert not supported with NCI db')
+def test_insert_unique(session):
+    db=session.session
+    kwargs={'variable': 'a2', 'mip': '6hrLev', 'model': 'c',
+            'experiment': 'd', 'ensemble': 'e','realm':  'realm'}
+    item, new= insert_unique(db,Instance,**kwargs)
+    assert new is True
+    assert type(item.id) is int 
+
+@pytest.mark.skip(reason='Insert not supported with NCI db')
+def test_update_item(session):
+    db=session.session
+    item_id=2
+    item = search_item(db,Version,**{'id': item_id})
+    assert item.is_latest is False
+    newvalues={'is_latest': True}
+    update_item(db, Version, item_id, newvalues)
+    item = search_item(db,Version,**{'id': item_id})
+    assert item.is_latest is True
+
+@pytest.mark.skip(reason='Insert not supported with NCI db')
+def test_add_bulk_items(session):
+    db=session.session
+    vid=1
+    num1=session.query(VersionFile).filter(VersionFile.version_id==vid).count() 
+    data=[('f1.nc','s2it4'),('f2.nc','3grt4'),('f3.nc','3abt4'),('f4.nc','3gr32')]
+    rows=[dict(filename=x[0], md5=x[1], version_id=vid) for x in data]
+    add_bulk_items(db,VersionFile,rows)
+    num2=session.query(VersionFile).filter(VersionFile.version_id==vid).count() 
+    assert num2==num1+4
