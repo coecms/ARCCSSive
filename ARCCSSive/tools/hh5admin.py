e2578fc8ce37a78e8ecbf36a534856293f6a4057
Scott Wales <scott.wales@unimelb.edu.au>
2017-06-16 11:09:42 +1000

    Add hh5_admin as a console script

--- ARCCSSive/tools/hh5admin.py
+++ ARCCSSive/tools/hh5admin.py
@@ -19,53 +19,74 @@ from ARCCSSive.db import connect, Session
 from ARCCSSive.model.hh5 import *
 
 from sqlalchemy.sql import func
-from sqlalchemy.types import Float
 
 import xml.etree.ElementTree
+import os
+from datetime import datetime
+from tabulate import tabulate
+import argparse
 
-connect(echo=True)
-session = Session()
+def main():
+    parser = argparse.ArgumentParser(description="Shows allocations for project hh5 (Climate LIEF)")
+    parser.add_argument('--debug', action='store_true',
+            help="Show database actions")
+    parser.add_argument('--requests', default='/g/data1/ua8/pxp581/requests_storage.xml',
+            help="XML file containing allocation requests")
+    args = parser.parse_args()
 
-# Get the id of the base path
-base = session.query(Path.pa_hash).filter_by(path='/g/data3/hh5/tmp').scalar()
+    connect(echo = args.debug)
+    session = Session()
 
-# Sum up the sizes of each level 5 directory
-sums = (session.query(Path.parents[5].label('parent_hash'),
-            func.sum(PosixFile.size).label('size'))
-        .select_from(PosixFile)
-        .join(PosixFile.path)
-        .filter(Path.parents[4] == base)
-        .group_by(Path.parents[5])).subquery()
+    # Get the id of the base path
+    base = session.query(Path.pa_hash).filter_by(path='/g/data3/hh5/tmp').scalar()
 
-# Convert the level 5 path hash to a path name
-q = session.query(Path.path, sums.c.size).select_from(sums).join(Path, Path.pa_hash == sums.c.parent_hash)
+    # Sum up the sizes of each level 5 directory
+    sums = (session.query(Path.parents[5].label('parent_hash'),
+                func.sum(PosixFile.size).label('size'))
+            .select_from(PosixFile)
+            .join(PosixFile.path)
+            .filter(Path.parents[4] == base)
+            .group_by(Path.parents[5])).subquery()
 
-requestsfile = '/g/data1/ua8/pxp581/requests_storage.xml'
-etree = xml.etree.ElementTree.parse(requestsfile).getroot()
+    # Convert the level 5 path hash to a path name
+    q = session.query(Path.path, sums.c.size).select_from(sums).join(Path, Path.pa_hash == sums.c.parent_hash)
 
-def process_path(path, size):
-    """
-    Compare the disk usage with the allocation
-    """
-    name = os.path.basename(path)
-    size_tb = size / 1024.0**4
-    messages = []
+    etree = xml.etree.ElementTree.parse(args.requests).getroot()
 
-    disks = etree.findall("disk[dirname='%s']"%name)
-    if len(disks) > 1:
-        messages.append('Multiple allocations for this directory')
+    def process_path(path, size):
+        """
+        Compare the disk usage with the allocation
+        """
+        name = os.path.basename(path)
+        size_tb = float(size) / 1024.0**4
+        messages = []
 
-    quota_tb = float(disks[0].find('size').text)
-    expire_date = datetime.strptime(disks[0].find('expire').text, "%Y-%m-%d")
-    managers = disks[0].find('managers').text
+        disks = etree.findall("disk[dirname='%s']"%name)
+        if len(disks) > 1:
+            messages.append('Multiple allocations for this directory')
 
-    if size_tb > quota_tb:
-        messages.append('Over quota')
+        try:
+            quota_tb = float(disks[0].find('size').text)
+            expire_date = datetime.strptime(disks[0].find('expire').text, "%Y-%m-%d")
+            managers = disks[0].find('managers').text
 
-    if expire_date > datetime.now():
-        messages.append('Allocation expired')
+        except IndexError:
+            expire_date = datetime.min
+            quota_tb = 0
+            managers = ''
+            messages.append('No allocation')
 
-    return [name, managers, size_tb, quota_tb, ', '.join(messages)]
-    
-for path, size in q:
-    print(process_path(path,size))
+        if size_tb > quota_tb:
+            messages.append('Over quota')
+
+        if expire_date > datetime.now():
+            messages.append('Allocation expired')
+
+        return [name, managers, size_tb, quota_tb, ', '.join(messages)]
+
+    print(tabulate(
+        [process_path(path, size) for path, size in q],
+        headers = ['Project', 'Managers', 'Size (Tb)', 'Quota (Tb)', 'Warnings']))
+
+if __name__ == '__main__':
+    main()
